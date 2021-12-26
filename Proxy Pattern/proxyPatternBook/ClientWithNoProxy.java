b7bdd148cdd6ee583a6649e9101b2dde98c871ec
vans <spentmny@nate.com>
2021-12-26 21:56:27 +0900

    Proxy Pattern 책 예시(No Proxy) 구현

---
+++ Proxy Pattern/proxyPatternBook/ClientWithNoProxy.java
@@ -0,0 +1,9 @@
+package proxyPatternBook;
+
+public class ClientWithNoProxy {
+    public static void main(String[] args) {
+        //프록시를 이요하지 않은 호출
+        Service service = new Service();
+        System.out.println(service.runSomething());
+    }
+}
---
+++ Proxy Pattern/proxyPatternBook/Service.java
@@ -0,0 +1,7 @@
+package proxyPatternBook;
+
+public class Service {
+    public String runSomething() {
+        return "서비스 짱!!!";
+    }
+}

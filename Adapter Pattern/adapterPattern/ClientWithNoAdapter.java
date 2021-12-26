a32aab4e421fe9b293b82a085f9c6f2980fa851a
vans <spentmny@nate.com>
2021-12-26 15:24:48 +0900

    어댑터 패턴 적용전 예시

---
+++ Adapter Pattern/adapterPattern/ClientWithNoAdapter.java
@@ -0,0 +1,11 @@
+package adapterPattern;
+
+public class ClientWithNoAdapter {
+    public static void main(String[] args) {
+        ServiceA sa1 = new ServiceA();
+        ServiceB sa2 = new ServiceB();
+
+        sa1.runServiceA();
+        sa2.runServiceB();
+    }
+}
